90bb7ee22cd40f577abbfe4c8601d6ce4e0ad678
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2020-02-01 11:54:37 -0800

    minor: adding descriptions.

--- setup.py
+++ setup.py
@@ -3,13 +3,16 @@ import os
 from setuptools import setup, find_packages
 
 base = os.path.dirname(os.path.abspath(__file__))
+with open(os.path.join(base, "README.md")) as fh:
+    long_description = fh.read()
 
 setup(
     name="pelican-export",
     setup_requires=["vcver"],
-    vcver={"path": base},
-    description="",
-    long_description="",
+    vcver={"path": base, "is_release": True},
+    description="Pelican plugin to export articles to other platforms.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     install_requires=["python-wordpress-xmlrpc", "pelican"],
     author="Yusuke Tsutsumi",
     author_email="yusuke@tsutsumi.io",
